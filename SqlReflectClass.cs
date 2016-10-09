b7f456ea63295756c7a87647600705d3cbd5a10a
weisingxcw <weisingxcw@outlook.com>
2016-10-09 14:20:12 +0800

    Eidt 2016.10.9
    Remove

---
+++ SqlReflectClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace Web.SQL
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        /// 获取SQL反射泛型集合
+        /// </summary>
+        /// <param name="connStr">数据库连接字符串</param>
+        /// <param name="sqlStr">要查询的T-SQL</param>
+        /// <returns></returns>
+        protected IList<T> sqlReflectClass<T>(string connStr, string sqlStr) where T : class,new()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connStr))
+            {
+                sqlConnection.Open();
+                using (SqlCommand cmd = new SqlCommand(sqlStr, sqlConnection))
+                {
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+
+                    IList<T> list = new List<T>();
+
+                    PropertyInfo[] classPropertyInfo = new T().GetType().GetProperties();
+                    
+                    while (dataReader.Read())
+                    {
+                        T delegateType = new T();
+                        foreach (var column in classPropertyInfo)
+                        {
+                            column.SetValue(delegateType, dataReader[column.Name], null);
+                        }
+                        list.Add(delegateType);
+                    }
+                    sqlConnection.Close();
+                    return list;
+                }
+            }
+        }
+    }
+
+}
